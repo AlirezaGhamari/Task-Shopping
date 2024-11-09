34ccbbc3785a03bc5976f05dd4fc1305300d71f7
Alireza <158749430+AlirezaGhamari@users.noreply.github.com>
2024-11-10 01:03:02 +0330

    12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
+import localFont from 'next/font/local';
+const myFont = localFont({
+  src: '../../public/fonts/Ketchup Manis Demo.ttf',
+  display: 'swap',
+})
+
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="ltr">
+    <html lang="en" dir="ltr" className={myFont.className}>
       <StoreProvider>
         <body className=" grid grid-cols-12 h-screen">
           <div className=" col-span-2 h-screen ">
